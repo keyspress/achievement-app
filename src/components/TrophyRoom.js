8654107fe99d80d2b04e32feed7fa00bada80b9a
Kyle <kylespresser@gmail.com>
2018-06-09 14:17:21 -0400

    style TrophyRoom

--- src/components/TrophyRoom.js
+++ src/components/TrophyRoom.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { TrophyCase } from './TrophyCase';
+import './trophyRoom.css';
 
 class TrophyRoom extends Component {
   render() {
     return (
-      <div>
-        <h1>Trophy Room Name</h1>
+      <div className="trophy-room-container">
+        <h1 className="trophy-room-header">Trophy Room Name</h1>
         <p>Description of Trophy Room</p>
         <TrophyCase />
       </div>
