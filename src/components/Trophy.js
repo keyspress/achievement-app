42e9aa5da0ec4b03c867101552bd798d7ca6625b
Kyle <kylespresser@gmail.com>
2018-06-09 14:43:22 -0400

    style Trophy

--- src/components/Trophy.js
+++ src/components/Trophy.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './trophy.css';
 
 export const Trophy = () => (
-  <div>
+  <div className="trophy-container">
     <h2>Trophy Name</h2>
     <p>Description of the trophy</p>
+    <p>Date Completed</p>
     <p>Relevent links</p>
   </div>
 )
