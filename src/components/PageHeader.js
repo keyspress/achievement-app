f32eecf35c4d95620e2cfe42aa48a5dedf0a49b1
Kyle <kylespresser@gmail.com>
2018-06-16 12:45:31 -0400

    clean up commented out code and unneeded css file

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -3,31 +3,12 @@ import { Header, Icon } from 'semantic-ui-react';
 
 import { pageHeaderContainerStyles, pageHeaderIconStyles, pageHeaderSubheaderStyles } from './styles/page-header-style';
 
-import './styles/page-header.css';
-
-// import { appColorBlack, appColorWhite } from './styles/theme';
-
-// const pageHeaderContainerStyles = {
-//   backgroundColor: appColorBlack,
-//   color: appColorWhite,
-//   display: 'flex',
-//   flexDirection: 'column',
-//   alignItems: 'center'
-// };
-
-// const pageHeaderIconStyles = { 
-//   paddingTop: '15px', 
-//   marginBottom: '15px' 
-// };
-
-// const pageHeaderSubheaderStyles = {
-//   color: appColorWhite
-// };
-
-export const PageHeader = () => <Header as="h1" style={pageHeaderContainerStyles}>
-           <Icon name="trophy" style={pageHeaderIconStyles} />
-           Achievement Board
-           <Header.Subheader style={pageHeaderSubheaderStyles}>
-             See all that you've accomplished
-           </Header.Subheader>
-         </Header>;
+export const PageHeader = () => (
+  <Header as="h1" style={pageHeaderContainerStyles}>
+    <Icon name="trophy" style={pageHeaderIconStyles} />
+    Achievement Board
+    <Header.Subheader style={pageHeaderSubheaderStyles}>
+      See all that you've accomplished
+    </Header.Subheader>
+  </Header>
+);
